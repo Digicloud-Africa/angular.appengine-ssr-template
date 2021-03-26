0fd10d925fef036be2ad5ef447ad7331da1f1f21
sizwe <sizwe@digicloud.africa>
2021-03-26 09:14:24 +0200

    install script enhancement

--- gcloud.new.project.sh
+++ gcloud.new.project.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 # init project and enable apis
-echo "This script assumes that you have npm and gcloud already installed. We will first install your new project init your project."
-
-npm install
+echo "This script assumes that you have npm and gcloud already installed. We will first init your Google Cloud project."
 
 gcloud init
 export PROJECT=$(gcloud info --format='value(config.project)')
+
+#enable firestore
 gcloud services enable firebase.googleapis.com
+
+#appengine
+gcloud app create --project=$PROJECT
 gcloud services enable cloudbuild.googleapis.com
 
 export SERVICE_ACC='appengine-firestore'
@@ -19,9 +22,11 @@ gcloud iam service-accounts keys create src/environments/firestore.json --iam-ac
 
 #replace the contents of these files to get different connection environments
 cp src/environments/firestore.json src/environments/firestore.prod.json
-
+echo "Now we will install build and deploy this codebase."
 # this project is set up so when deploying to appengine you are only deploying
 # the /dist folder which is the output of the build folder.
 # thus you must always build before deploy.
+npm install
+npm run build:protos
 npm run build:ssr
 gcloud app deploy
